b4fc0d51db56ae306044245061867b2b659f9bc7
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-06-04 21:54:19 +0530

    10 PM

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -1,19 +1,31 @@
 /* eslint-disable node/no-unpublished-import */
+/* eslint-disable node/no-missing-import */
+/* eslint-disable no-unused-vars */
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/dist/types";
-// import { developmentChains } from "../helper-hardhat-config";
+import { developmentChains } from "../helper-hardhat-config";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts, network } = hre;
+  const { deployments, getNamedAccounts, network, ethers } = hre;
+  const BASE_FEE = ethers.utils.parseEther("0.25");
+  const GAS_PRICE_LINK = 1e9;
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
+  const _args = [BASE_FEE, GAS_PRICE_LINK];
 
   if (chainId === 31337) {
     log(`Local network detected! Deploying mocks...`);
     // Deploy a VRF Mock
+    await deploy("VRFCoordinatorV2Mock.sol", {
+      from: deployer,
+      log: true,
+      args: _args,
+    });
+    log("Mocks deployed");
+    log(`--------------------------------------`);
   }
 };
 
 export default func;
-func.tags = ["raffle"];
+func.tags = ["all", "mocks"];
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,9 +1,13 @@
+/* eslint-disable node/no-unpublished-import */
+import { ethers } from "hardhat";
+import "";
+
 export interface networkConfigItem {
   name?: string;
   subscriptionId?: string;
   gasLane?: string;
   keepersUpdateInterval?: string;
-  raffleEntranceFee?: string;
+  entranceFee?: any;
   callbackGasLimit?: string;
   vrfCoordinatorV2?: string;
 }
@@ -16,6 +20,15 @@ export const networkConfig: networkConfigInfo = {
   4: {
     name: "rinkeby",
     vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    gasLane:
+      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    entranceFee: ethers.utils.parseEther("0.01"),
+    subscriptionId: "0",
+  },
+  31337: {
+    name: "localhost",
+    gasLane:
+      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
   },
 };
 
