1e4925820d9eecf38652fe7ff546fbe328f3102a
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-06-04 20:14:49 +0530

    Push

---
+++ deploy/00-deploy-mocks.ts
@@ -0,0 +1,19 @@
+/* eslint-disable node/no-unpublished-import */
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+// import { developmentChains } from "../helper-hardhat-config";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  if (chainId === 31337) {
+    log(`Local network detected! Deploying mocks...`);
+    // Deploy a VRF Mock
+  }
+};
+
+export default func;
+func.tags = ["raffle"];
